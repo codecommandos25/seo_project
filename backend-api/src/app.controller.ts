7b6365ba7fdc4ade7d457fc4ff22c244a28e4056
prabhav <spratap7088@gmail.com>
2025-10-22 22:28:15 +0530

    update build

--- backend-api/src/app.controller.ts
+++ backend-api/src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import { join } from 'path';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
