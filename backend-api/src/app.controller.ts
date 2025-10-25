60579a00c4aa7eab14dcf49e2926a964e15866fa
prabhav <spratap7088@gmail.com>
2025-10-25 09:43:50 +0530

    update

--- backend-api/src/app.controller.ts
+++ backend-api/src/app.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Response } from 'express';
-import { join } from 'path';
+// import { Response } from 'express';
+// import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/admin-panel/*')
-  getAdmin(@Res() res: Response) {
-    res.sendFile(join(__dirname, '..', 'website', 'index.html'));
-  }
+  // @Get('/admin-panel/*')
+  // getAdmin(@Res() res: Response) {
+  //   res.sendFile(join(__dirname, '..', 'website', 'index.html'));
+  // }
 }
--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -5,14 +5,14 @@ import { ThirdPartyApisModule } from './third-party-apis/third-party-apis.module
 import { ConfigModule } from '@nestjs/config';
 import { YoutubeApisModule } from './youtube-apis/youtube-apis.module';
 import { MetaApisModule } from './meta-apis/meta-apis.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'website'),
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'website'),
+    // }),
     ThirdPartyApisModule,
     ConfigModule.forRoot(),
     YoutubeApisModule,
